9990b50d159d2bb6e75ac4aadc69dd0d38ed77d3
Mom1881990 <111296090+Mom1881990@users.noreply.github.com>
2025-12-15 21:59:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,21 @@ function toggleMusic() {
   m.paused ? m.play() : m.pause();
 }
 
-// Countdown
-const date = new Date("2025-12-21T11:00:00").getTime();
+// COUNTDOWN
+const weddingDate = new Date("2025-12-21T11:00:00").getTime();
 setInterval(() => {
   const now = new Date().getTime();
-  const d = date - now;
+  const d = weddingDate - now;
   if (d < 0) return;
-  const days = Math.floor(d / (1000*60*60*24));
-  const hours = Math.floor((d / (1000*60*60)) % 24);
-  const mins = Math.floor((d / (1000*60)) % 60);
-  document.getElementById("timer").innerHTML =
-    `${days} ngày • ${hours} giờ • ${mins} phút`;
+
+  document.getElementById("days").innerText = Math.floor(d / (1000*60*60*24));
+  document.getElementById("hours").innerText = Math.floor((d / (1000*60*60)) % 24);
+  document.getElementById("minutes").innerText = Math.floor((d / (1000*60)) % 60);
+  document.getElementById("seconds").innerText = Math.floor((d / 1000) % 60);
 }, 1000);
+
+// LIGHTBOX
+function openImg(src) {
+  document.getElementById("lightbox").style.display = "flex";
+  document.getElementById("lightImg").src = src;
+}
